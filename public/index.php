ce59ad098e1b2a489f58c030a9e89863dc14e4d5
hoochicken <mareike.riegel@ql.de>
2022-12-30 14:33:06 +0100

    fix (cors) Cors problem solved, letters can be loaded by vze frontend

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+header("Access-Control-Max-Age", "3600");
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header("Access-Control-Allow-Credentials", "true");
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
