2caafa5959954cb426d5d00dd55bbd42756a57b3
hoochicken <mareike.riegel@ql.de>
2022-12-31 11:30:31 +0100

    feat (test) Tests added

--- src/Controller/WordController.php
+++ src/Controller/WordController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Repository\LetterRepository;
 use App\Repository\WordRepository;
+use Exception;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class WordController extends ApiController
 {
@@ -17,16 +19,30 @@ class WordController extends ApiController
 
     public function wordAction(Request $request, WordRepository $wordRepository)
     {
-        $words = $this->getRandomElements($wordRepository->findAll());
-        array_walk($words, function(&$item) { $item = $item->getTitle(); });
-        return $this->respond(['words' => $words]);
+        try {
+            $words = $this->getRandomElements($wordRepository->findAll());
+            array_walk($words, function (&$item) {
+                $item = $item->getTitle();
+            });
+            return $this->respond(['words' => $words]);
+        } catch (Exception $e) {
+            throw new HttpException('Something went wrong when getting the words');
+        }
+
     }
 
     public function wordsByLetterAction(Request $request, WordRepository $wordRepository, LetterRepository $letterRepository, $letters = '', $length = 0)
     {
-        $words = $this->getRandomElements($wordRepository->findByLetters($letters, $length));
-        array_walk($words, function(&$item) { $item = $item->getTitle(); });
-        return $this->respond(['words' => $words]);
+        try {
+            $words = $this->getRandomElements($wordRepository->findByLetters($letters, $length));
+            array_walk($words, function (&$item) {
+                $item = $item->getTitle();
+            });
+            return $this->respond(['words' => $words, 'result' => 'ok']);
+        } catch (Exception $e) {
+            throw new HttpException($e->getCode(), $e->getMessage() . 'Something went wrong when getting the words');
+        }
+
     }
 
     private function getRandomElements(array $array, int $num = 1000): array
@@ -34,7 +50,7 @@ class WordController extends ApiController
         if (0 === count($array)) return [];
         if (1 === count($array)) return $array;
         $keysOfArray = array_rand($array, min($num, count($array)));
-        $array = array_filter($array, function($k) use ($keysOfArray) {
+        $array = array_filter($array, function ($k) use ($keysOfArray) {
             return in_array($k, $keysOfArray);
         }, ARRAY_FILTER_USE_KEY);
         sort($array);
---
+++ tests/api/WordCest.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Tests;
+use App\Tests\ApiTester;
+class WordCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function getWordListTest(ApiTester $I)
+    {
+        // $I->amHttpAuthenticated('service_user', '123456');
+        $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
+        $I->sendGet('/words/abcde');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseContains('{"result":"ok"}');
+
+    }
+}
